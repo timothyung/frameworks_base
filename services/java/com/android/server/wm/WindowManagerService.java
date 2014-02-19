8013fcc02a0fa811b64d167fff7d7b121783581e
Yevgen Pronenko <yevgen.pronenko@sonymobile.com>
2014-02-19 10:33:07 +0100

    Avoid duplication of AppWindowToken in created Task
    When WindowManagerService creates a new Task, it passes atoken to the
    Task constructor. In this case atoken is added to mAppTokens list by
    the Task constructor and then it is added manually again by calling
    newTask.mAppTokens.add(atoken). As a result, the same atoken is present
    in mAppTokens list twice.
    
    When another window token is added to the list, it may be placed in
    beetwen duplicated tokens and corresponding window will be hidden from
    the screen by corresponding duplicated windows.
    From user perspective it means that some windows will not be displayed
    on the screen.
    
    The issue is fixed by avoid adding atoken to mAppTokens list manually
    when it was added already in Task constructor.
    
    Change-Id: I10628e68186160fffdde07beb7d84ab3cecb7051

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -3488,8 +3488,9 @@ public class WindowManagerService extends IWindowManager.Stub
             Task newTask = mTaskIdToTask.get(groupId);
             if (newTask == null) {
                 newTask = createTask(groupId, oldTask.mStack.mStackId, oldTask.mUserId, atoken);
+            } else {
+                newTask.mAppTokens.add(atoken);
             }
-            newTask.mAppTokens.add(atoken);
         }
     }
 
