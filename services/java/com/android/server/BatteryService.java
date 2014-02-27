f028e416be96fd0a9e08d6745c03cf2abe9ed3f4
ctfrommn <chadclough@gmail.com>
2014-02-27 17:45:43 +0100

    Because 90% isnt full

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -769,7 +769,7 @@ public final class BatteryService extends Binder {
                 }
             } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                     || status == BatteryManager.BATTERY_STATUS_FULL) {
-                if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
+                if (status == BatteryManager.BATTERY_STATUS_FULL || level > 99) {
                     // Battery is full or charging and nearly full
                     mBatteryLight.setColor(mBatteryFullARGB);
                 } else {
